50f32e78d6a5ee33bd0a9103ed15a4a4e85c2441
Tobias Doe <developer@onmyown.at>
2021-03-19 16:36:32 +0100

    npm install tailwind

---
+++ js/script.js
@@ -0,0 +1,53 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/js/script"],{
+
+/***/ "./resources/js/bootstrap.js":
+/*!***********************************!*\
+  !*** ./resources/js/bootstrap.js ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+try {} catch (e) {}
+
+/***/ }),
+
+/***/ "./resources/js/script.js":
+/*!********************************!*\
+  !*** ./resources/js/script.js ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js"); // jQuery should work here xD
+
+
+console.log('mint init()'); // window.Fortawesome = require('@fortawesome/fontawesome-pro/js/all');
+
+/***/ }),
+
+/***/ "./resources/scss/styles.scss":
+/*!************************************!*\
+  !*** ./resources/scss/styles.scss ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!*******************************************************************!*\
+  !*** multi ./resources/js/script.js ./resources/scss/styles.scss ***!
+  \*******************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! /Users/tobias/Developer/gitlab/mint-tailwind/resources/js/script.js */"./resources/js/script.js");
+module.exports = __webpack_require__(/*! /Users/tobias/Developer/gitlab/mint-tailwind/resources/scss/styles.scss */"./resources/scss/styles.scss");
+
+
+/***/ })
+
+},[[0,"/js/manifest","/js/vendor"]]]);
+//# sourceMappingURL=script.js.map
